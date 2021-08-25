8b40b4227453bec1c5a83a3866bd68d76e03bd2c
Rashgaron <45242200+Rashgaron@users.noreply.github.com>
2021-08-25 16:36:32 +0200

    Added edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,31 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
+    def edit
+        @article = Article.find(params[:id])     
+    end
     def new
         #Creamos un nuevo articulo vacío
         #Una variable con @, es una variable que se pasa del controlador a la vista
         #Una variable sin @, es una variable que solo existe localmente
         @article = Article.new
-        @article.title = "Demo"
     end
 
     def create
         @article = Article.create(title: params[:article][:title], status: params[:article][:status])
         render json: @article
+    end
+    def update
+        @article = Article.find(params[:id])
+        @article.update(title: params[:article][:title], content: params[:article][:content], status: params[:article][:status])
+        render json: @article
+        redirect_to @article
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to root_path
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/bienvenida", to: "home#index"
+
+  root to: "home#index"
+
   get "articles/new", to: "articles#new"
+  get "articles/:id/edit", to: "articles#edit"
   post "articles", to:"articles#create"
+
+  patch "/articles/:id", to: "articles#update", as: :article
+  delete "/articles/:id", to: "articles#destroy"
+  get "articles/:id", to: "articles#show"
 end
